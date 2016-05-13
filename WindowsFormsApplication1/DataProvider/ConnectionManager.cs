ae4512201ef7786e567e995f0e3919309963495d
Federico Catinello <fcatinello@gmail.com>
2016-05-13 00:01:43 -0300

    Fix connection manager & add query helper to execute and read querys

---
+++ WindowsFormsApplication1/DataProvider/ConnectionManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Security;
+
+namespace WindowsFormsApplication1.DataProvider
+{
+    class ConnectionManager
+    {
+
+        private static String CONNECTION_STRING = "gd1C2016";
+        private static String SCHEMA = "NET_A_CERO";
+        private static String DATABASE = "GD1C2016";
+        private static String USER_ID = "gd";
+
+        /// <summary>
+        /// Singleton attribute
+        /// </summary>
+        private static ConnectionManager instance;
+
+        public static ConnectionManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ConnectionManager();
+                }
+                return instance;
+            }
+        }
+
+        //DB connection instance
+        private SqlConnection gd_connection;
+
+        //Get DB connection
+        public SqlConnection getConnection()
+        {
+            if (gd_connection == null)
+            {
+                connect();
+            }
+            return gd_connection;
+        }
+
+
+        //Get DB credentials
+        public SqlCredential getCredentials()
+        {
+            SecureString password = new SecureString();
+            password.AppendChar('g');
+            password.AppendChar('d');
+            password.AppendChar('2');
+            password.AppendChar('0');
+            password.AppendChar('1');
+            password.AppendChar('6');
+
+            return new SqlCredential(USER_ID, password);
+        }
+
+        //Connect to DB
+        public void connect()
+        {
+            gd_connection = new SqlConnection(CONNECTION_STRING, getCredentials());
+            gd_connection.Open();
+
+            new SqlCommand("USE [" + DATABASE + "] ", gd_connection).ExecuteNonQuery();
+        }
+
+
+        //Close DB connection
+        public void close()
+        {
+            gd_connection.Close();
+        }
+
+        public String getSchema()
+        {
+            return SCHEMA;
+        }
+
+    }
+
+}
---
+++ WindowsFormsApplication1/DataProvider/QueryHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.DataProvider
+{
+    class QueryHelper
+    {
+
+        /// <summary>
+        /// Singleton attribute
+        /// </summary>
+        private static QueryHelper instance;
+
+        public static QueryHelper Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new QueryHelper();
+                }
+                return instance;
+            }
+        }
+
+
+        private SqlDataReader reader;
+
+        /// <summary>Execute a query with a return value (functions or selects)</summary>
+        /// <param name="query">Query to be executed</param>
+        /// <returns>Returns the reader with the results of the query</returns>
+        public SqlDataReader exec(String query)
+        {
+            SqlCommand command = new SqlCommand(query, ConnectionManager.Instance.getConnection());
+            reader = command.ExecuteReader();
+            return reader;
+        }
+
+        public bool readFrom(SqlDataReader reader_parameter)
+        {
+            return reader_parameter.Read();
+        }
+
+        /// <summary>Close the reader opened with the previous method (if the reader is not closed it will fail in the next execution)</summary>
+        public void closeReader()
+        {
+            reader.Close();
+        }
+
+    }
+}
